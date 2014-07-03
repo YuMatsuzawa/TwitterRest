bb6b1bc8722cf95cf49e8d6d46c86ce4ed9db679
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2014-07-03 12:56:25 +0900

    new testing package and class added. twitter4j lib updated to 4.0.2. some deprecated methods not accounted.

---
+++ TwitterRest/src/matz/twitter/search/keyword/KeywordSearchTest.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package matz.twitter.search.keyword;
+
+import java.util.List;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**testing twitter API of keyword search.
+ * @author Matsuzawa
+ *
+ */
+public class KeywordSearchTest {
+	public static ConfigurationBuilder cb = new ConfigurationBuilder();
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		cb.setDebugEnabled(true)
+		.setOAuthConsumerKey("KuG34YZ202uEZCHgHQhQ")
+		.setOAuthConsumerSecret("5R9nqymC0Zl5pF5KSDr80gCOTHqkaPS9sehrGoMpI")
+		.setOAuthAccessToken("352840258-QrB0NPIYNtGiNUt3vnetmnMumnppJETnvccdZVJa")
+		.setOAuthAccessTokenSecret("PkuqzpWNCWglAGKtMfUeg1UVCRQqjqesmlOuyrVA")
+	    .setJSONStoreEnabled(true);
+		
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter tw = tf.getInstance();
+		
+		Query query = new Query("小保方");
+		query.count(100);
+		
+		try {
+			QueryResult qr = tw.search(query);
+			
+			List<Status> res = qr.getTweets();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
