74d5882af1f9d11aa021fde1b915741175901a49
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2014-07-04 13:46:57 +0900

    old tweet search tried and failed. basically v1.1 search still won't gather tweets older than a week.

--- TwitterRest/src/matz/twitter/search/KeywordSearchTest.java
+++ TwitterRest/src/matz/twitter/search/KeywordSearchTest.java
@@ -32,16 +32,17 @@ public class KeywordSearchTest {
 			Query query = null;
 			QueryResult qr = null;
 			//List<Status> res = new ArrayList<Status>();
-			long maxid = Long.MAX_VALUE;
+//			long maxid = Long.MAX_VALUE;
+			long maxid = Long.parseLong("359085410497986560");
 			int extraTry = 3;
 			int secondsUntilReset = 300;
 			int total = 0;
 			System.out.println("total\tOldest\t(requiredTime)");
 			do {
 				do {
-					query = new Query("小保方");
+					query = new Query("選挙");
 					query.count(100);
-					query.setUntil("2014-05-30");
+//					query.setUntil("2014-05-30");
 					query.setMaxId(maxid);
 					try{
 						do {
