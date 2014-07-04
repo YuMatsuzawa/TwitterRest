f0990f752af3daee008113df66e3248f1af93d61
Yu <Yu@192.168.84.214>
2014-07-04 12:19:37 +0900

    gmail lib and class

---
+++ TwitterRest/src/matz/api/google/mail/send/GoogleSendmailTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package matz.api.google.mail.send;
+
+/**
+ * @author Yu
+ *
+ */
+public class GoogleSendmailTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
